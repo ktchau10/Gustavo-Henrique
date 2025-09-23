b798df795616e0ac6c5e66dbdc154a855ab421bb
Gustavo Fernandes <gh5857750@gmail.com>
2025-09-23 17:54:59 -0300

    Delete prog directory

--- prog/administrador/admin_auth_check.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-// Se a sessão não existe ou não está como logada, redireciona para o login
-if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
-    header('Location: admin_login.php');
-    exit;
-}
